6435a747d6b20e9cb9f8d03feeb8ad6d34fa42ec
Blake Holman <baholman@ksu.edu>
2019-05-05 15:13:18 -0500

    adds tests to run

--- 3way-mpi/allrun_mpi.sh
+++ 3way-mpi/allrun_mpi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=1 --cpus-per-task=16 mpi_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=16 mpi_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=32 mpi_shell_script.sh
--- 3way-openmp/allrun_openmp.sh
+++ 3way-openmp/allrun_openmp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=1 --cpus-per-task=16 openmp_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=2 --cpus-per-task=16 openmp_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=4 --cpus-per-task=16 openmp_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=8 --cpus-per-task=16 openmp_shell_script.sh
+sbatch --constraint=elves --time=5:00:00 --mem-per-cpu=5G --nodes=16 --cpus-per-task=16 openmp_shell_script.sh
