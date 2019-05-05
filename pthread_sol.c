a49a64eb506d43a745df729c123c314e4ec012c2
Cullen Povilonis <cullenp@ksu.edu>
2019-05-05 13:50:16 -0500

    HAHAHHAHAHAHA FIXED BLAKE'S ERRORS

--- pthread_sol.c
+++ pthread_sol.c
@@ -5,12 +5,11 @@
 #include <math.h>
 #include <pthread.h>
 
-//#define NUM_THREADS 12
+#define NUM_THREADS 12
 
 #define MAXCHAR 2200
-FILE *fp;
-int NUM_THREADS=12;
-//char** file_array;
+
+char** file_array;
 int file_lines;
 
 void freeArray(int **arr, int length){
@@ -19,15 +18,28 @@ void freeArray(int **arr, int length){
 	}
 	free(arr);
 }
-
+//Print; Least Common Substring
+//Source: https://www.geeksforgeeks.org/print-longest-common-substring/
+//Changes: removes /n, no longer stores dynamic array on the stack
 void printLCSubStr(char* X, char* Y, int m, int n,int l1,int l2)
 {
-
+    // Create a table to store lengths of longest common
+    // suffixes of substrings.   Note that LCSuff[i][j]
+    // contains length of longest common suffix of X[0..i-1]
+    // and Y[0..j-1]. The first row and first column entries
+    // have no logical meaning, they are used only for
+    // simplicity of program
+//    int LCSuff[m + 1][n + 1]; //segfaults on large strings
+	//for a big array
 	int **LCSuff = malloc((m+1) * sizeof(int *));;
 	for(int k=0; k<(m+1);k++)
 		LCSuff[k] =(int*)malloc((n+1)*sizeof(int));
     // To store length of the longest common substring
     int len = 0;
+ 
+    // To store the index of the cell which contains the 
+    // maximum value. This cell's index helps in building 
+    // up the longest common substring from right to left.
     int row, col;
  
    // Following steps build LCSuff[m+1][n+1] in bottom up fashion. 
@@ -68,9 +80,9 @@ void printLCSubStr(char* X, char* Y, int m, int n,int l1,int l2)
         row--;
         col--;
     }
-	//printf("LCSuff");
 	freeArray(LCSuff, m);  
-//remove newline char
+    //remove newline char
+    //src: https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
 	size_t length;
     if((length = strlen(resultStr)) >0){
 		if(resultStr[length-1] == '\n')
@@ -83,42 +95,22 @@ void printLCSubStr(char* X, char* Y, int m, int n,int l1,int l2)
    
 }
 
-
-
-void *LCS_runner(void *id){
+void LCS_runner(int id){
 	int s1,s2;
-
-	int startPos = ((long)id) * ceil((double)file_lines / NUM_THREADS);
+	int startPos = ((int) id) * ceil((double)file_lines / NUM_THREADS);
 	int endPos = startPos + ceil((double)file_lines / NUM_THREADS);	
-
-	char** file_array;
-	file_array = malloc(sizeof(char*) * file_lines);
-	int i;
-	for(i =0; i < file_lines; i++){
-		if(ferror(fp) || feof(fp)){
-			break;			
-		}
-		file_array[i] = malloc(sizeof(char) * MAXCHAR);
-		fgets(file_array[i], MAXCHAR, fp);
-		//printf("%s",file_array[i]);
-	}
-	
+//	if (endPos < file_lines)
+//		endPos = file_lines-1;
 	int line1 = startPos;
 	int line2 = line1 + 1;
 	for(int i =startPos; (i < endPos) &&(i+1 < file_lines); i++){
 		s1 = strlen(file_array[i]);
 		s2 = strlen(file_array[i+1]);
 			
-		//thread create
 		printLCSubStr(file_array[i], file_array[i+1], s1, s2, line1, line2);
-	//printf("I am thread: %d\n",(int)id);	
 		line1 = line2; //update lines
 		line2++;
 	}
-		//free the array
-	printf("file_array");
-	freeArray((int **)file_array, file_lines);
-	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
@@ -128,39 +120,36 @@ int main(int argc, char *argv[])
 	double elapsedTime;
 	int myVersion = 1;
 	
-	//threads
-	int i, rc;
-	pthread_t threads[NUM_THREADS];
-	pthread_attr_t attr;
-	void *status;
-	
+	int i;
 	gettimeofday(&t1, NULL);
-	
+								
+	//read file
+	FILE *fp;
+	//int s1,s2;
+
 	int problem_size;
 	char* filename;
 	if((argc == 2)){
 		filename = argv[1]; //get name from argument
 	}
-	else if(argc == 4){
+	else if(argc == 3){
 		filename = argv[1];
 		problem_size = strtol(argv[2], NULL, 10);
-		NUM_THREADS = strtol(argv[3], NULL, 10);
 	}
 	else{
-		printf("Usage: ./program <file> | <problem size> <thread count>\n");
+		printf("Usage: ./program <file> <problem size>\n");
         return 1;
 	}
 	
     fp = fopen(filename, "r");
     if (fp == NULL){
         printf("Could not open file: %s\n",filename);
-		printf("Usage: ./program <file> | <problem size> <thread count>\n");
+		printf("Usage: ./program <file> | <problem size>\n");
         return 1;
     }
     
 	//read file into array
 	int ch = 0;
-	
 	while(!feof(fp))
 	{
 	  ch = fgetc(fp);
@@ -169,36 +158,31 @@ int main(int argc, char *argv[])
 		file_lines++;
 	  }
 	}
-	
 	if((argc > 2) && (problem_size < file_lines)){
 		file_lines = problem_size;
 	}
 	rewind(fp);
 
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+	file_array = malloc(sizeof(char*) * file_lines);	
 	
+	for(i =0; i < file_lines; i++){
+		if(ferror(fp) || feof(fp)){
+			break;			
+		}
+		file_array[i] = malloc(sizeof(char) * MAXCHAR);
+		fgets(file_array[i], MAXCHAR, fp);
+		//printf("%s",file_array[i]);
+	}
+	//process array in lcs function
 
 	for (i = 0; i < NUM_THREADS; i++ ) {
-	      rc = pthread_create(&threads[i], &attr, LCS_runner, (void *)((long)i));
-	      if (rc) {
-	        printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		  }
+		LCS_runner(i);
 	}
 	
-	//thread join
-	pthread_attr_destroy(&attr);
-	for(i=0; i<NUM_THREADS; i++) {
-	     rc = pthread_join(threads[i], &status);
-	     if (rc) {
-		   printf("ERROR; return code from pthread_join() is %d\n", rc);
-		   exit(-1);
-	     }
-	}
-
+	//free the array
+	freeArray((int **)file_array,file_lines);
 	//close file
-	fclose(fp);
+    fclose(fp);
 	
 	//print time
 	gettimeofday(&t2, NULL);
@@ -206,7 +190,6 @@ int main(int argc, char *argv[])
 	elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; //sec to ms
 	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
 	printf("DATA, %d, %s, %f\n", myVersion, getenv("SLURM_CPUS_ON_NODE"),  elapsedTime);
-	pthread_exit(NULL);
-	free(threads);
+
   return 0;
 }
--- runtest.sh
+++ runtest.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 #files: wiki_sample.txt, test_big.txt, test_shortline.txt
-fname='/mnt/c/Users/bgust/OneDrive/Documents/cis520/proj4/wiki_dump.txt'
+fname='/home/cullen/cis520/LetsGitIt/proj4/wiki_dump.txt'
 outname='log.txt'
 nthreads=1000
 searchtext='DATA'
 psize=50
 rm $outname
 
-echo "\nOPEN MP" >> $outname
+echo "OPEN MP" >> $outname
 gcc -fopenmp -o openmp openmp_sol.c -lm
 #./openmp.out $fname  $psize $nthreads | grep -c $searchtext >> $outname
-./openmp $fname  $psize $nthreads | sort -g >> $outname
+./openmp $fname $psize $nthreads | sort -g >> $outname
 
-echo "\nMPI" >> $outname
+echo "MPI" >> $outname
 mpicc -o mpiSol mpi_sol.c -lm 
-./mpiSol $fname  $psize >> $outname
+./mpiSol $fname $psize >> $outname
 #mpirun -np $nthreads mpi_sol.c $fname $psize| grep $searchtext >> $outname
 
-echo "\nPTHREADS" >> $outname
+echo "PTHREADS" >> $outname
 gcc -pthread -o pthreadrun pthread_sol.c -lm 
-./pthreadrun $fname  $psize $nthreads >> $outname
+./pthreadrun $fname $psize >> $outname
 #mpirun -np $nthreads mpi_sol.c $fname $psize| grep $searchtext >> $outname
 
 echo "Output: $outname"
