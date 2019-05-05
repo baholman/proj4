7b11ee2e9bc64c25298409165a7a16dad53f46e9
Blake Holman <baholman@ksu.edu>
2019-05-05 14:46:37 -0500

    edits bash test file

--- runtest.sh
+++ runtest.sh
@@ -9,18 +9,18 @@ psize=50
 rm $outname
 
 echo "OPEN MP" >> $outname
-gcc -fopenmp -o openmp openmp_sol.c -lm
+gcc -fopenmp -o 3way-openmp/openmp 3way-openmp/openmp_sol.c -lm
 #./openmp.out $fname  $psize $nthreads | grep -c $searchtext >> $outname
-./openmp $fname $psize $nthreads | sort -g >> $outname
+./3way-openmp/openmp $fname $psize $nthreads | sort -g >> $outname
 
 echo "MPI" >> $outname
-mpicc -o mpiSol mpi_sol.c -lm 
-./mpiSol $fname $psize >> $outname
+mpicc -o 3way-mpi/mpiSol 3way-mpi/mpi_sol.c -lm 
+./3way-mpi/mpiSol $fname $psize >> $outname
 #mpirun -np $nthreads mpi_sol.c $fname $psize| grep $searchtext >> $outname
 
 echo "PTHREADS" >> $outname
-gcc -pthread -o pthreadrun pthread_sol.c -lm 
-./pthreadrun $fname $psize >> $outname
+gcc -pthread -o 3way-pthread/pthreadrun 3way-pthread/pthread_sol.c -lm 
+./3way-pthread/pthreadrun $fname $psize >> $outname
 #mpirun -np $nthreads mpi_sol.c $fname $psize| grep $searchtext >> $outname
 
 echo "Output: $outname"
